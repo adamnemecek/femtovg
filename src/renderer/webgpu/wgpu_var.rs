550042f847a66aefe7eafbfae2ae913d604dc373
adamnemecek <adamnemecek@gmail.com>
2021-03-21 09:02:51 -0700

    deref

--- src/renderer/webgpu/wgpu_var.rs
+++ src/renderer/webgpu/wgpu_var.rs
@@ -1,5 +1,21 @@
 
 pub struct WGPUVar<T> {
-    
+    inner: wgpu::Buffer,
     ph: std::marker::PhantomData<T>,
+}
+
+impl<T> WGPUVar<T> {
+
+}
+
+impl<T> std::ops::Deref for WGPUVar<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        let slice = self.inner.slice(..);
+        let z = slice.get_mapped_range();
+        let z = z.as_ref();
+        unsafe {
+            (z.as_ptr() as *const T).as_ref().unwrap()
+        }
+    }
 }
